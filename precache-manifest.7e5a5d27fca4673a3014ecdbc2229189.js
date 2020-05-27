2556e1fbe8ce9f2b018dfd4ddb2f2c69685a9ede
Adam Dziendziel <218922+fadehelix@users.noreply.github.com>
2020-05-27 20:26:03 +0200

    Updates

---
+++ precache-manifest.7e5a5d27fca4673a3014ecdbc2229189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61df379fb5f320ca0ba04adc79dde573",
+    "url": "/momi/index.html"
+  },
+  {
+    "revision": "4023f129e5302c79f0b9",
+    "url": "/momi/static/css/2.2908a31a.chunk.css"
+  },
+  {
+    "revision": "029c36f2c65870b6625d",
+    "url": "/momi/static/css/main.74208ab6.chunk.css"
+  },
+  {
+    "revision": "4023f129e5302c79f0b9",
+    "url": "/momi/static/js/2.e2f6e759.chunk.js"
+  },
+  {
+    "revision": "4369a283cb702f6e1b40c254f3619ee6",
+    "url": "/momi/static/js/2.e2f6e759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029c36f2c65870b6625d",
+    "url": "/momi/static/js/main.cb321be7.chunk.js"
+  },
+  {
+    "revision": "65990b665e66cb498e87",
+    "url": "/momi/static/js/runtime-main.b05cd144.js"
+  }
+]);
